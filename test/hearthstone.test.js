3b44f7ec19a2aee198672a5b180d2fd778872139
David Konsumer <konsumer@jetboystudio.com>
2024-04-24 04:33:18 -0700

    test that shows how to use sub-messages

--- test/hearthstone.test.js
+++ test/hearthstone.test.js
@@ -27,3 +27,9 @@ test('Get raw sub-message, including meta, with getPath', () => {
   expect(d.dataByteLength).toEqual(15243)
   expect(d.sub.length).toEqual(42)
 })
+
+test('Get value inside another sub-object', () => {
+  const d = getPath(raw, pb, '1.2.4:raw')
+  expect(getPath(d.sub, pb, '1:string')).toEqual('com.blizzard.wtcg.hearthstone')
+  expect(getPath(d.sub, pb, '5:string')).toEqual('Hearthstone')
+})
